6255e3edf48e3551a89a992f765b8039ae9687dc
Ariel Darmon <19586672+ArielDarmon@users.noreply.github.com>
2024-01-29 15:21:44 +0200

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -30,8 +30,9 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
+            
             case 'RED_EXPENSE':
-                const red_expenses = state.expenses.map((currentExp)=> {
+                            const red_expenses = state.expenses.map((currentExp)=> {
                     if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
                         currentExp.cost =  currentExp.cost - action.payload.cost;
                         budget = state.budget + action.payload.cost
@@ -43,6 +44,7 @@ export const AppReducer = (state, action) => {
                     ...state,
                     expenses: [...red_expenses],
                 };
+            
             case 'DELETE_EXPENSE':
             action.type = "DONE";
             state.expenses.map((currentExp)=> {

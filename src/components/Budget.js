9409fd27d4d862687359e84709ea7621ee08fbe3
Ariel Darmon <19586672+ArielDarmon@users.noreply.github.com>
2024-01-29 15:22:44 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,9 +3,23 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const [newBudget, setNewBudget] = useState(budget);
+    const maxLimit = 20000;
+    
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        if(event.target.value>maxLimit) {
+            alert("This value cannot exceed the limit of "+ Intl.NumberFormat().format(maxLimit));
+            setNewBudget(maxLimit);
+        } else if(event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spendings");
+            setNewBudget(totalExpenses);
+        } else {
+            setNewBudget(event.target.value);
+        }
     }
     return (
 <div className='alert alert-secondary'>
